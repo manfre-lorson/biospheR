88252f01ccb531336e2c8f203f137cc49f4738b4
manfre-lorson <manfre-lorson@posteo.de>
2021-03-09 15:28:53 +0100

    adjust to dem thueringen

--- Geodata/Annex2/Elevation/xyz2tif.py
+++ Geodata/Annex2/Elevation/xyz2tif.py
@@ -15,21 +15,21 @@ def main(file):
     input is full path to file"""
     
     try:
-        os.system('gdal_translate -a_srs EPSG:32632 \"{}\" \"{}\"'.format(file, file[:-3]+'tif'))
+        os.system('gdal_translate -a_srs ESRI:102329 \"{}\" \"{}\"'.format(file, file.split(os.sep)[-1][:-3]+'tif'))
     except:
         print('error: ', file)
 
 # set in path to folder with .xyz files        
-path = r'R:\Rh_Rasterdaten\DGM_Raster\He\DGM_Hessen\DGM 1\*.xyz'
+path = r'R:\Rh_Rasterdaten\DGM_Raster\Th\dgm1_2017_xyz_clip\*.xyz'
 # change directory to folder
-os.chdir(r'R:\Rh_Rasterdaten\DGM_Raster\He\DGM_Hessen\DGM 1')
+os.chdir(r'R:\Rh_Datenmanagement\Geodaten\Annex2\Elevation\DGM1\thueringen')
 
 # get all files as list
 files = glob(path)
 
-# run main definition on 4 cores for all files
+# run main definition on 6 cores for all files
 if __name__ == "__main__":
-    pool = Pool(4)
+    pool = Pool(6)
     for bar in pool.imap_unordered(main, files):
         foo = bar
     pool.terminate()
